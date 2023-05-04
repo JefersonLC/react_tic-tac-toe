22f5f0411c27234e0530d3e4019f39eb4226dc9e
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-03 20:29:28 -0500

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Square from './components/Square';
 import './styles/App.css';
+import Modal from './components/Modal';
 
 const TURNS = {
   FIRST_PLAYER: 'x',
@@ -23,6 +24,12 @@ export default function App() {
   const [turn, setTurn] = useState(TURNS.FIRST_PLAYER);
   const [winner, setWinner] = useState(null);
 
+  /**
+   * Verifica si hay un ganador.Si hay ganador lo retorna
+   * y si no retorna null
+   * @param {[]} boardToCheck Tablero
+   * @returns {string | null}
+   */
   function checkWinner(boardToCheck) {
     for (let combo of WINNER_COMBOS) {
       const [a, b, c] = combo;
@@ -36,21 +43,45 @@ export default function App() {
     return null;
   }
 
+  /**
+   * Cambia el estado de la tabla cada vez que se
+   * marca una casilla
+   * @param {number} index Índice de la casilla marcada
+   * @returns {void}
+   */
   function changeBoard(index) {
+    // Si ya hay un elemento en la casilla o si ya hay
+    // un ganador no se hace nada
     if (board[index] || winner) return;
 
+    // No mutar el arreglo original
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
 
+    // Se cambia al turno del siguiente jugador
     const newTurn =
       turn === TURNS.FIRST_PLAYER ? TURNS.SECOND_PLAYER : TURNS.FIRST_PLAYER;
     setTurn(newTurn);
 
+    // Se verifica si hay ganador
     const newWinner = checkWinner(newBoard);
     if (newWinner) setWinner(newWinner);
+    else if (checkEndGame(newBoard)) setWinner(false);
   }
 
+  /**
+   * Verifica si hay un empate
+   * @param {[]} boardToCheck
+   * @returns {boolean}
+   */
+  function checkEndGame(boardToCheck) {
+    return boardToCheck.every((square) => square !== null);
+  }
+
+  /**
+   * Resetea el juego
+   */
   function resetGame() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.FIRST_PLAYER);
@@ -58,17 +89,17 @@ export default function App() {
   }
 
   return (
-    <main className="app">
-      <h1 className="title">Tic Tac Toe</h1>
+    <main className='app'>
+      <h1 className='title'>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset Game</button>
-      <div className="board">
+      <div className='board'>
         {board.map((square, index) => (
           <Square key={index} index={index} changeBoard={changeBoard}>
             {square}
           </Square>
         ))}
       </div>
-      <div className="turn">
+      <div className='turn'>
         <Square isSelected={turn === TURNS.FIRST_PLAYER}>
           {TURNS.FIRST_PLAYER}
         </Square>
@@ -76,6 +107,8 @@ export default function App() {
           {TURNS.SECOND_PLAYER}
         </Square>
       </div>
+
+      <Modal winner={winner} reset={resetGame} />
     </main>
   );
 }
